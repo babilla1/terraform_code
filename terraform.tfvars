dc185121b232c789c8e76f41870e008e5173b2ea
EdgeTech <edgetech@EdgeTechs-MacBook-Pro.local>
2023-07-01 11:12:11 +0100

    babilla

---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+my_region  = "us-east-2"
+profile    = "default"
+topic_name = "sns-dev"
+endnpoint  = "babillaprosper3@gmail.com"
---
+++ vars.tf
@@ -0,0 +1,32 @@
+
+
+variable "my_region" {
+  type    = string
+  default = "us-east-1"
+
+}
+variable "profile" {
+  default = "profile"
+
+
+}
+variable "topic_name" {
+  default = "topic_name"
+
+}
+variable "protocol_type" {
+  default = "email"
+
+}
+variable "endpoint" {
+  default = "babillaprosp@dmail.com"
+
+}
+variable "tags" {
+  default = "babilla"
+
+}
+variable "team" {
+    default = "dev"
+  
+}
